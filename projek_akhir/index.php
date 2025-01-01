317eca82b1000333dfe16f502b385006c7113ce5
hiIgooTgar <igotegarprambudhy128945@gmail.com>
2025-01-02 00:25:34 +0700

    SBD - Projek Akhir - Update Code

--- projek_akhir/index.php
+++ projek_akhir/index.php
@@ -1,11 +1,6 @@
 <?php
 include "./koneksi/config.php";
 session_start();
-
-if (!isset($_SESSION['id_users'])) {
-    echo "<script>alert('Anda harus login dahulu');
-    window.location.href = 'login.php'</script>";
-}
 ?>
 
 <!DOCTYPE html>
@@ -15,23 +10,73 @@ if (!isset($_SESSION['id_users'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home - Srawung Roso</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/home.css">
     <link rel="stylesheet" href="./assets/css/fonts.css">
 </head>
 
 <body>
-    <header>
-        <nav class="navbar">
-            <div class="logo">
-                <a href="./">Srawung Roso</a>
-            </div>
-            <div class="nav-list">
-                <a href="./" class="nav-link">Home</a>
-                <a href="" class="nav-link"></a>
-            </div>
-        </nav>
-    </header>
-    <h1><?= $_SESSION['nama'] ?></h1>
-    <h1><?= $_SESSION['alamat'] ?></h1>
+
+    <?php
+    if (!isset($_SESSION['id_users']) && !isset($_SESSION['role']) == 0 || !isset($_SESSION['role']) == 1) {
+    ?>
+        <header>
+            <nav class="navbar">
+                <div class="logo">
+                    <a href="./"><span>Srawung</span>Roso</a>
+                </div>
+                <div class="nav-list">
+                    <a href="./" class="nav-link">Home</a>
+                    <a href="login.php" class="nav-link">Masuk</a>
+                    <a href="registrasi.php" class="nav-link">Daftar</a>
+                </div>
+            </nav>
+        </header>
+    <?php } else { ?>
+        <?php if ($_SESSION['role'] == 0) { ?>
+            <header>
+                <nav class="navbar">
+                    <div class="logo">
+                        <a href="./"><span>Srawung</span>Roso</a>
+                    </div>
+                    <div class="nav-list">
+                        <a href="./" class="nav-link">Home</a>
+                        <a href="src/data-master.php" class="nav-link">Data Master</a>
+                        <p class="nav-link">Hi, <?= $_SESSION['nama'] ?></p>
+                        <a href="logout.php" class="nav-link">Log Out</a>
+                    </div>
+                </nav>
+            </header>
+        <?php } ?>
+
+
+        <?php if ($_SESSION['role'] == 1) { ?>
+            <header>
+                <nav class="navbar">
+                    <div class="logo">
+                        <a href="./"><span>Srawung</span>Roso</a>
+                    </div>
+                    <div class="nav-list">
+                        <a href="./" class="nav-link">Home</a>
+                        <a href="src/menu.php" class="nav-link">Menu</a>
+                        <p class="nav-link">Hi, <?= $_SESSION['nama'] ?></p>
+                        <a href="logout.php" class="nav-link btn-logout">Log Out</a>
+                    </div>
+                </nav>
+            </header>
+        <?php } ?>
+    <?php } ?>
+
+    <section class="hero" id="home">
+        <main class="content">
+            <h1>Selamat Datang di <span>Srawung</span> Roso</h1>
+            <h2>Merasakan Harmoni Rasa dan Suasana Jawa</h2>
+            <p>Di Srawung Roso, kami menghadirkan cita rasa autentik Jawa yang memanjakan lidah, dipadukan dengan suasana yang hangat dan penuh kenangan. Temukan kelezatan di setiap sajian kami.</p>
+        </main>
+    </section>
+
+
+    <script src="./assets/js/main.js"></script>
 </body>
 
 </html>
